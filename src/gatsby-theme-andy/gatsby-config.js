be2bf978e8e6f849523b9eec43d0c3d1cfa236e9
Vincent Yang <vincenty.376@gmail.com>
2020-08-02 12:11:46 -0700

    Revert; add in local changes for gatsby theme

---
+++ src/gatsby-theme-andy/gatsby-config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = ({
+  notesDirectory = 'content/',
+  notesFileExtensions = ['.md', '.mdx'],
+  noteTemplate = path.join(__dirname, 'src/templates/note.js'),
+  additionalNoteTypes = {},
+  rootPath = '/',
+  rootNote = 'about',
+  linkifyHashtags = false,
+  hideDoubleBrackets = false,
+  mdxOtherwiseConfigured = false,
+}) => ({
+  siteMetadata: {
+    title: 'My Notes',
+  },
+  plugins: [
+    {
+      resolve: '@aengusm/gatsby-theme-brain',
+      options: {
+        notesDirectory,
+        notesFileExtensions,
+        noteTemplate,
+        additionalNoteTypes,
+        rootPath,
+        rootNote,
+        linkifyHashtags,
+        hideDoubleBrackets,
+        mdxOtherwiseConfigured,
+      },
+    },
+    {
+        resolve: '@arvindballa/gatsby-theme-andy',
+    }
+  ],
+});
